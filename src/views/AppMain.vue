a9914b8b464ad329a07b2fb1a4ea9b3f8512b1ef
ragabima12 <ragabima9g@gmail.com>
2021-01-21 18:10:41 +0700

    [ADD] Adding some features

--- src/views/AppMain.vue
+++ src/views/AppMain.vue
@@ -1,5 +1,27 @@
 <template>
   <div>
+    <v-menu
+      :position-x="accountDialog.position_x"
+      :position-y="accountDialog.position_y"
+      v-model="accountDialog.isShowed"
+      :min-width="accountDialog.width"
+      absolute
+      offset-y
+    >
+      <v-list>
+        <a href="#" class="app-nolink" @click="test">
+          <v-list-item
+            v-for="(menuItem, menuItemindex) of menuItems.menus"
+            :key="menuItemindex"
+          >
+            <v-list-item-title>
+              <h4 class="app-heading-thin">{{ menuItem.title }}</h4>
+            </v-list-item-title>
+          </v-list-item>
+        </a>
+      </v-list>
+    </v-menu>
+
     <v-app-bar color="#081120" dark class="px-6" height="82">
       <v-toolbar-title class="mr-8">
         <h3
@@ -9,30 +31,26 @@
           INTENS <span class="app-text-thin">Console</span>
         </h3>
       </v-toolbar-title>
-      <v-btn plain color="white"
+      <v-btn plain color="white" @click="activePage = `dashboard`"
         ><h4 class="app-text-white app-heading-thin">Dashboard</h4></v-btn
       >
-      <v-btn plain color="white"
-        ><h4
-          class="app-text-white app-heading-thin"
-          @click="activePage = `presence`"
-        >
-          Presensi
-        </h4></v-btn
+      <v-btn plain color="white" @click="activePage = `presence`"
+        ><h4 class="app-text-white app-heading-thin">Presensi</h4></v-btn
       >
-      <v-btn plain color="white"
-        ><h4
-          class="app-text-white app-heading-thin"
-          @click="activePage = `studentData`"
-        >
-          Data Siswa
-        </h4></v-btn
+      <v-btn plain color="white" @click="activePage = `studentData`"
+        ><h4 class="app-text-white app-heading-thin">Data Siswa</h4></v-btn
       >
       <v-btn plain color="white"
         ><h4 class="app-text-white app-heading-thin">Konfigurasi</h4></v-btn
       >
       <v-spacer></v-spacer>
-      <v-chip color="accent" pill class="pr-6 pl-4 py-6 app-chip-full-rounded">
+      <v-chip
+        color="accent"
+        pill
+        class="pr-6 pl-4 py-6 app-chip-full-rounded"
+        ref="account"
+        @click="AccountDialog"
+      >
         <v-avatar color="primary" size="56" class="mr-4">
           <img
             lazy-src="https://picsum.photos/id/11/10/6"
@@ -44,8 +62,8 @@
       </v-chip>
     </v-app-bar>
     <div class="mt-12">
-      <v-row>
-        <v-col cols="3" offset="1" v-if="activePage !== `dashboard`"
+      <v-row v-if="activePage !== `dashboard`">
+        <v-col cols="3" offset="1"
           ><v-card class="mx-auto" width="300">
             <v-list>
               <v-list-item>
@@ -121,6 +139,12 @@
           </v-row>
         </v-col>
       </v-row>
+
+      <v-row v-else>
+        <v-col cols="10" offset="1">
+          <h1>OK!</h1>
+        </v-col>
+      </v-row>
     </div>
   </div>
 </template>
@@ -131,6 +155,12 @@ import StudentData from "@/views/StudentData";
 export default {
   data: () => ({
     activePage: "dashboard",
+    accountDialog: {
+      isShowed: false,
+      width: 200,
+      position_x: 0,
+      position_y: 0,
+    },
     admins: [
       ["Management", "mdi-account-multiple-outline"],
       ["Settings", "mdi-cog-outline"],
@@ -141,7 +171,31 @@ export default {
       ["Update", "mdi-update"],
       ["Delete", "mdi-delete"],
     ],
+    menuItems: {
+      title: "",
+      menus: [
+        {
+          title: "OK!",
+        },
+        {
+          title: "HEI!",
+        },
+      ],
+    },
   }),
+  methods: {
+    AccountDialog() {
+      const accountChip = this.$refs["account"].$el;
+      const { x, y, width } = accountChip.getBoundingClientRect();
+      this.accountDialog.position_x = x;
+      this.accountDialog.position_y = y + 64;
+      this.accountDialog.width = width;
+      this.accountDialog.isShowed = !this.accountDialog.isShowed;
+    },
+    test() {
+      alert("OK!");
+    },
+  },
   components: {
     Presence,
     StudentData,
